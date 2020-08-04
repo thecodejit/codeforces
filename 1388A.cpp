047684cb8ef2883defe07fe8ac1b747c60b64df4
soumyajit <rickdey446@gmail.com>
2020-08-04 23:06:56 +0530

    Captain Flint and Crew Recruitment

---
+++ 1388A.cpp
@@ -0,0 +1,27 @@
+//Author :: Soumyajit Dey (TheCodejit)
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define magic ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std; 
+ int main(){
+     magic;
+     int t;
+     cin >> t;
+     while(t--){
+         int n;
+         cin >> n;
+          if(n <= 30){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+            if(n == 36 || n == 40 || n == 44){
+                cout << 6 << ' ' << 10 << ' ' << 15 << ' ' << n - 31 << endl;
+            }
+            else{
+                cout << 6 << ' ' << 10 << ' ' << 14 << ' ' << n - 30 << endl;
+            }
+     }
+ }
+ }

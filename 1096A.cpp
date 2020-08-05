46e6c164879ada211e95f79ee7003e412cad789f
soumyajit <rickdey446@gmail.com>
2020-08-05 21:09:14 +0530

    Find Divisible

---
+++ 1096A.cpp
@@ -0,0 +1,21 @@
+//Author :: Soumyajit Dey (TheCodejit)
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define magic ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+ 
+ 
+using namespace std;
+int main()
+{
+    magic;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int l,r;
+        cin >> l >> r;
+        
+        cout << l << ' ' << l*2 << '\n';
+    }
+}

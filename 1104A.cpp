116f9192e365c5a267d0245a6cde39c3efa41fd6
soumyajit <rickdey446@gmail.com>
2020-08-05 21:12:34 +0530

    Splitting into digits

---
+++ 1104A.cpp
@@ -0,0 +1,16 @@
+//Author :: Soumyajit Dey (TheCodejit)
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define magic ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+ 
+using namespace std;
+int main()
+{
+    magic;
+    int n;
+    
+    cin >> n;
+    cout << n << '\n';
+    for(int i=1; i<=n; i++){cout << 1 << ' ';}
+}
